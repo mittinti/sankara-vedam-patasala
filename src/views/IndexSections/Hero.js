b04a36ee56dd2bebebbc25b68d49684bd538deed
SFCC Trainer <co.sfcc@cloudodyssey.co.uk>
2024-07-15 22:52:53 +0200

    color changes

--- src/views/IndexSections/Hero.js
+++ src/views/IndexSections/Hero.js
@@ -15,7 +15,7 @@ class Hero extends React.Component {
           {/* Hero for FREE version */}
           <section className="section section-hero section-shaped">
             {/* Background circles */}
-            <div className="shape shape-style-1 shape-default">
+            <div className="shape shape-default">
               <span className="span-150" />
               <span className="span-50" />
               <span className="span-50" />
@@ -85,21 +85,7 @@ class Hero extends React.Component {
               </div>
             </Container>
             {/* SVG separator */}
-            <div className="separator separator-bottom separator-skew zindex-100">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-              >
-                <polygon
-                  className="fill-white"
-                  points="2560 0 2560 100 0 100"
-                />
-              </svg>
-            </div>
+            
           </section>
         </div>
       </>
